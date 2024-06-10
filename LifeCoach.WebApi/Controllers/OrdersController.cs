432c5edc892f7d5b6ec477bb43cb4289d7a6d124
Aysegul <karadan.ay7@gmail.com>
2024-06-10 21:40:52 +0300

    controller update

--- LifeCoach.WebApi/Controllers/OrdersController.cs
+++ LifeCoach.WebApi/Controllers/OrdersController.cs
@@ -1,47 +1,44 @@
 ﻿using LifeCoach.Application;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LifeCoach.WebApi;
-[Authorize]
-[Microsoft.AspNetCore.Components.Route("api/[controller]")]
-[ApiController]
-public class OrdersController:ControllerBase
 
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
 {
     private readonly ISender _mediatr;
+
     public OrdersController(ISender mediatr)
     {
         _mediatr = mediatr;
     }
-     [HttpGet("{id:guid}")]
-        public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
-        {
 
-            return Ok(await _mediatr.Send(new OrderGetByIdQuery(id), cancellationToken));
-        }
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return Ok(await _mediatr.Send(new OrderGetByIdQuery(id), cancellationToken));
+    }
 
-    
-        [HttpPost]
-        public async Task<IActionResult> AddAsync(OrderAddCommand command,CancellationToken cancellationToken)
-        {
-            return Ok(await _mediatr.Send(command, cancellationToken));
-        }
+    [HttpPost]
+    public async Task<IActionResult> AddAsync(OrderAddCommand command, CancellationToken cancellationToken)
+    {
+        return Ok(await _mediatr.Send(command, cancellationToken));
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
-        {
-            return Ok(await _mediatr.Send(new OrderGetAllQuery(), cancellationToken));
-        }
-       
-        [AllowAnonymous]
-        [HttpDelete("{id:guid}")]
-        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
-        {
-            return Ok(await _mediatr.Send(new OrderDeleteCommand(id), cancellationToken));
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return Ok(await _mediatr.Send(new OrderGetAllQuery(), cancellationToken));
+    }
 
-
+    [AllowAnonymous]
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        return Ok(await _mediatr.Send(new OrderDeleteCommand(id), cancellationToken));
+    }
 }
